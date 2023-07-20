fd0967b7a969ebf3a34d753521641391344da938
arclyte1 <arclyte18@gmail.com>
2023-07-20 18:02:31 +0300

    Arguments required handle added

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,19 +5,25 @@ fun main(args: Array<String>) {
 
     val code = args.find {
         it.startsWith("--code=")
-    }?.substringAfter("--code=")!!
+    }?.substringAfter("--code=")
 
     val date = args.find {
         it.startsWith("--date=")
-    }?.substringAfter("--date=")!!
+    }?.substringAfter("--date=")
 
-    val rate = GetCurrencyRateUseCase().invoke(code, date)
-
-    if (rate != null) {
-        println("${rate.code} (${rate.name}): ${rate.rate}")
-        exitProcess(0)
-    } else {
-        println("Unexpected error occurred")
+    if (code == null || date == null) {
+        println("Arguments --code=XXX and --date=YYYY-MM-DD required")
         exitProcess(-1)
+    } else {
+
+        val rate = GetCurrencyRateUseCase().invoke(code, date)
+
+        if (rate != null) {
+            println("${rate.code} (${rate.name}): ${rate.rate}")
+            exitProcess(0)
+        } else {
+            println("Unexpected error occurred")
+            exitProcess(-1)
+        }
     }
 }
